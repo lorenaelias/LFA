59fd6e92d8345461595c5fa3b61594d2056fa43d
breno-cn <38513013+breno-cn@users.noreply.github.com>
2019-06-14 02:28:24 -0300

    Add files via upload
    Mudança no retorno do efecho.

--- T3/AFP.py
+++ T3/AFP.py
@@ -1,3 +1,6 @@
+"""
+    TODO: Falta concertar o efecho, a ideia é retornar uma lista de tuplas que contém (Estado, Nova pilha alterada, Topo antigo)
+"""
 class AFP:
     def __init__(self, nomeArq):
         with open(nomeArq, 'r') as arq:
@@ -42,32 +45,98 @@ class AFP:
 
     # TODO: Efecho de um estado com pilha
 
-    #     return resultado
+    """
+    * Se a regra tiver tamnaho 1 (regra == X), retornar pilha inalterada
+    * Se a regra tiver tamanho 1, mas for o simbolo especial (regra == '&'), desempilhar a pilha e retornar ela junto com o pop
+    * Se a regra tiver tamanho 2, mas retornar a pilha após empilhar o simbolo recebido
+    * Caso seja empilhado, o antigo topo será irrelevante, nesse caso será retornado None na tupla
+    """
 
-    def efecho(self, estado):
-        efecho = [estado]                                                   #? Entender melhor a função dessa variável
-        resultado = [estado]                                                #* Resultado da função, inclui o estado de entrada
-        dic = {}                                                            #* Nova função delta que exclui o elemento da pilha das tuplas
+    def alteraPilha(self, regra, simbolo, pilha):
+        if regra == '&':
+            antigoTopo = pilha.pop()
+            return (pilha, antigoTopo)
 
+        elif len(regra) == 1:
+            return (pilha, pilha[-1])
+
+        elif len(regra) == 2:
+            pilha.append(simbolo)
+            return (pilha, None)
+
+    def efecho(self, estado, pilha):
+        regraInicial = self.getRegraPilha('&', estado)                                        #* (Estado, Nova pilha, Antigo topo)
+        efecho    = [ (estado, pilha, regraInicial) ]                                         #* Pilha de contexto, usada na recursão
+        resultado = [ (estado, pilha, regraInicial) ]
+        dic = {}
+        
         for i in self.delta:
-            dic[(i[0], i[1])] = (self.delta[i])[0]                          #* (Simbolo, Estado) -> Novo Estado
+            dic[ (i[0], i[1]) ] = (self.delta[i])[0]
 
-        while efecho != []:                                                 #* Enquanto não houver novos estados a serem processados...
-            naoProcessado = (efecho.pop())                                  #* Desempilha um estado não processado
-            print('Topo efecho: ', naoProcessado, type(naoProcessado))
-            if ('&', naoProcessado) in dic:                                 #* Se existir uma E-transição a partir do estado pego...
-                for i in dic[('&', naoProcessado)]:                         #* Percorra todas as possibilidades
-                    if i not in efecho and i not in resultado:              #* Se for encontrado um estado novo para todos os contextos...
-                        efecho.append(i)
-                    if i not in resultado:                                  #* Adicionar ao conjunto de resultados
-                        resultado.append(i)
-            elif naoProcessado not in resultado:                            #* Caso o estado pego ainda não tenha sido adicionado...
-                resultado.append(naoProcessado)
-        print('Resultado efecho -> ', resultado)
+        while efecho != []:
+            naoProcessado = efecho.pop()
+            print('Nao processado -> ', naoProcessado, '\t', type(naoProcessado))
+            if ('&', naoProcessado[0]) in dic:
+                for i in dic[ ('&', naoProcessado[0]) ]:
+                    print('i -> ', i)
+                    if i not in efecho and i not in resultado:
+                        regra = self.getRegraPilha('&', naoProcessado[0])
+                        print('Regra -> ', regra)
+                        #efecho.append( (i[1],  regra[0][0], regra[0][1]) )
+                        for j in regra:
+                            efecho.append( (i[1], j[0], j[1]) )
+                    elif i not in resultado:
+                        regra = self.getRegraPilha('&', naoProcessado[0])
+                        #resultado.append( (i[1], regra[0][0], regra[0][1]) )
+                        for j in regra:
+                            resultado.append( (i[1], j[0], j[1]) )
+            elif naoProcessado[0] not in resultado[0]:
+                print('funciona', naoProcessado)
+                resultado.append( (naoProcessado[1][0], naoProcessado[2], self.getRegraPilha('&', naoProcessado[1][0])) )
+        print('Resultado -> ', resultado)
         return resultado
 
+
+    def getRegraPilha(self, simbolo, estado):
+        resultado = []
+        for i in self.delta:
+            if simbolo == i[0] and estado == i[1]:
+                # print(i)
+                # return i[2]
+                resultado.append(self.delta[i])
+        print('regraPilha -> ', resultado)
+        return resultado
+
+    #? TESTAR ALTERAR O RETORNO DO EFECHO PARA ({Estados}, {Regras de pilha correspondentes})
+    # def efecho(self, estado):
+    #     efecho = [estado]
+    #     resultado = [estado]
+    #     dic = {}
+
+    #     for i in self.delta:
+    #         dic[(i[0], i[1])] = (self.delta[i])[0]
+
+    #     print('dic\n', dic)
+        
+    #     while efecho != []:
+    #         naoProcessado = (efecho.pop())
+    #         print('Topo efecho = ', naoProcessado)
+    #         if ('&', naoProcessado) in dic:
+    #             for i in dic[('&', naoProcessado)]:
+    #                 print('i -> ', i)
+    #                 if i not in efecho and i not in resultado:
+    #                     efecho.append( (i[1], self.getRegraPilha(i[0], i[1])) )
+    #                 if i not in resultado:
+    #                     resultado.append( (i[1], self.getRegraPilha(i[0], i[1])) )
+
+    #                 elif naoProcessado not in resultado:
+    #                     resultado.append( (naoProcessado, self.getRegraPilha(i[0], i[1])) )
+
+    #     print('Resultado efecho -> ', resultado)
+    #     return resultado
     
     def percorreCadeia(self, cadeia, estadoAtual, pilha):
+        #* CONDIÇÃO DE PARADA DA RECURSÃO
         if cadeia == '':
             return estadoAtual in self.F or pilha == [] #or pilha == [self.Z]   #! pilha == [self.Z] é usado apenas pra teste
         
@@ -75,26 +144,26 @@ class AFP:
         verEstado = (carAtual, estadoAtual, pilha[-1])                      #* Tupla a ser processada pela função delta: (Simbolo, Estado, Topo da pilha)
         print('verEstado -> ', verEstado)
 
-        if verEstado in self.delta:                                         #* Caso exista a tupla dentro do conjunto de regras
+        if verEstado in self.delta:                                         #* Caso exista a tupla dentro do conjunto de regras...
             proxEstado = self.delta[verEstado]                              #* Aplicando delta
-            transicao = self.efecho(proxEstado[0])
-            print('transicao -> ', transicao)
             print('ProxEstado -> ', proxEstado, end='\n\n\n')
 
-            if proxEstado[1] == '&': pilha.pop()                            #* Caso a regra de delta seja &, desempilhar o topo da pilha
-            if len(proxEstado[1]) == 2: pilha.append(proxEstado[1][1])      #* Caso o tamanho seja 2 (XX, YY, XZ, ...), empilhar o segundo simbolo
+            if proxEstado[1] == '&': pilha.pop()                            #* Caso a regra de delta seja &, desempilhar o topo da pilha...
+            if len(proxEstado[1]) == 2: pilha.append(proxEstado[1][1])      #* Caso o tamanho seja 2 (XX, YY, XZ, ...), empilhar o segundo simbolo...
             
             #! Regras em que o topo permanece inalterado são escritas no arquivo como um único caractere, aquele que se espera
             #! que permaneça no topo
 
             print('pilha -> ', pilha)
-            for estado in proxEstado:                                       #* Percorra o conjunto de estados gerado por delta
-                if estado not in proxEstado[0]:                             #? Entender melhor a condição de backtracking!
-                    print('Backtracking!')
-                    print(verEstado, ' -> ', end='')
-                print(estado, '\t', type(estado))
+            for estado in proxEstado:                                                                     #* Percorra o conjunto de estados gerado por delta
+                for q in self.efecho(estado[0]):                                                          #* Percorra o efecho do estado escolhido
+                    print('proxEstado[0] = ', proxEstado[0])
+                    if q not in proxEstado and pilha[-1] == verEstado[2]:                                 #? Entender melhor a condição de backtracking!
+                        print('Backtracking!')
+                        print(verEstado, ' -> ', end='')
+                        print(q, '\t', type(estado))
 
-                if self.percorreCadeia(cadeia[1:], estado[0], pilha):
-                    return True
+                    if self.percorreCadeia(cadeia[1:], q, pilha):
+                        return True
 
         return False
--- T3/main.py
+++ T3/main.py
@@ -4,5 +4,8 @@ import sys
 p = AFP(sys.argv[1])                                                #  Nome do arquivo que contêm o AFP, passado pelo terminal
 cadeia = sys.argv[2]                                                #  Cadeia a ser percorrida
 print(p)
-print(p.efecho(p.q0))
+# print(p.efecho(p.q0))
+print(p.getRegraPilha('0', p.q0))
+print(p.alteraPilha('&', '1', ['Z', '0', '0']))
+p.efecho(p.q0, p.pilha)
 # print(p.percorreCadeia(cadeia, p.q0, p.pilha))
