43424e3f0ba191b38c1de1b79b66f71b35f9ec2d
lorenaelias <39497142+lorenaelias@users.noreply.github.com>
2019-06-14 00:50:02 -0300

    percorrimento com alguns problemas

--- T3/APF.py
+++ T3/APF.py
@@ -17,13 +17,17 @@ class APF:
 
 
             for k in range(len(matriz)) :
-                if (matriz[k][2] == '[]') :
+                if matriz[k][2] == '[]' :
                     self.delta[matriz[k][0], matriz[k][1]] = []
-                else :
+                else:
                     aux = matriz[k]
                     aux = aux[3 :]
-                    self.delta[matriz[k][0], matriz[k][1], matriz[k][2]] = [tuple(aux)]
-
+                    temp = []
+                    if (matriz[k][0], matriz[k][1], matriz[k][2]) in self.delta:
+                        temp = self.delta[matriz[k][0], matriz[k][1], matriz[k][2]]
+                    self.delta[matriz[k][0], matriz[k][1], matriz[k][2]] = [tuple(aux)] + temp
+                    # esse ultimo if serve para nao sobrescrever uma transicao quando existir
+                    # mais de uma com a mesma key
             arq.close()
 
         else:
@@ -46,12 +50,62 @@ class APF:
                 return False
         return True
 
+    def efecho(self, est):
+        efecho = [est]
+        resultado = [est]
+        while(efecho != []):
+            est1 = efecho.pop()
+            qq = any
+            print("topo efecho: ",est1)
+            if (est1, '&', qq) in self.delta:
+                for i in self.delta[(est1, '&', qq)]:
+                    if i not in efecho and i not in resultado:
+                        efecho.append(i)
+                    if i not in resultado:
+                        resultado.append(i)
+            else:
+                if est1 not in resultado:
+                    resultado.append(est1)
+        return resultado
+
+    def alteraPilha(self, estAt, a, pilhaAt, d2):
+        novaPi = pilhaAt
+        if d2 == '&':
+            novaPi.pop()
+        # elif d2 == pilhaAt.top():
+        #     #nao modifica a pilha
+        else:
+            novaPi.append(d2.top())
+        return novaPi
+
+
     def percorreAPF(self, sequencia, qAt, pilhaAt):
         if sequencia == "":
-            return self.pertence(qAt,self.F)
+            if qAt in self.F:
+                return True
+            for i in self.F:
+                if i in self.efecho(qAt):
+                    return True
         else:
-            #TODO: RECURSAO DA ACEITAÇÃO
-            return
+            a = sequencia[0]
+            checaestado = (qAt, a, pilhaAt)
+            print(checaestado, ' -> ', end='')
+
+            if checaestado in self.delta:
+                estAt = checaestado[0]
+                for (d1, d2) in self.delta[(qAt,'&',pilhaAt.top())]:
+                    estAt2 = d1
+                    piAt = self.alteraPilha(estAt2, a, pilhaAt, d2)
+                    self.percorreAPF(sequencia,d1,piAt)
+
+                    if (d1, '&', pilhaAt):
+                        estAt = d1
+                        for (d1, d2) in self.delta[(qAt, '&', pilhaAt.top())]:
+                            estAt2 = d1
+                            piAt = self.alteraPilha(estAt2, a, pilhaAt, d2)
+                            self.percorreAPF(sequencia,estAt2,piAt)
+
+        return False
 
     def printAPF(self):
         print('\n\n-------\033[1;34mAUTOMATO DE PILHA POR ESTADO FINAL\033[0;0m-------\n')
